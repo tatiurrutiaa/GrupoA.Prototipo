7d55fe988c97667892cc14c250a0ba3f7929577e
FERNANDO JOSE MARTINEZ CKACKA <58MA43406426@campus.economicas.uba.ar>
2024-05-13 10:09:27 -0300

    2

---
+++ Empaquetar/EmpaquetarForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace GrupoA.Prototipo.Empaquetado
+{
+    partial class EmpaquetarForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            StockList = new ListView();
+            MercaderiaCol = new ColumnHeader();
+            CodPedidoCol = new ColumnHeader();
+            CantidadCol = new ColumnHeader();
+            CuitCol = new ColumnHeader();
+            TrasnportistaCol = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // StockList
+            // 
+            StockList.Columns.AddRange(new ColumnHeader[] { MercaderiaCol, CodPedidoCol, CantidadCol, CuitCol, TrasnportistaCol });
+            StockList.GridLines = true;
+            StockList.Location = new Point(15, 43);
+            StockList.Name = "StockList";
+            StockList.Size = new Size(710, 137);
+            StockList.TabIndex = 0;
+            StockList.UseCompatibleStateImageBehavior = false;
+            // 
+            // EmpaquetarForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(734, 450);
+            Controls.Add(StockList);
+            Name = "EmpaquetarForm";
+            Text = "EmpaquetarForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView StockList;
+        private ColumnHeader MercaderiaCol;
+        private ColumnHeader CodPedidoCol;
+        private ColumnHeader CantidadCol;
+        private ColumnHeader CuitCol;
+        private ColumnHeader TrasnportistaCol;
+    }
+}
---
+++ Empaquetar/EmpaquetarForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrupoA.Prototipo.Empaquetado
+{
+    public partial class EmpaquetarForm : Form
+    {
+        public EmpaquetarForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
