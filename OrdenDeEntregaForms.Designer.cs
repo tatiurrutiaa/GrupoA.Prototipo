a2d16556f57b6b1707b1eeb655fcad195aa244a6
NICOLÁS AGUSTÍN LÓPEZ <lopeznico99@gmail.com>
2024-05-21 17:46:09 -0300

    1

--- OrdenDeEntregaForms.Designer.cs
+++ OrdenDeEntregaForms.Designer.cs
@@ -29,20 +29,9 @@
         private void InitializeComponent()
         {
             groupBoxOrdenPreparación = new GroupBox();
-            checkedListBox2 = new CheckedListBox();
-            checkedListBox1 = new CheckedListBox();
-            textBox2 = new TextBox();
-            label8 = new Label();
-            label5 = new Label();
-            Cantid = new Label();
-            label6 = new Label();
-            textBoxFechaEntrega = new TextBox();
-            label4 = new Label();
             comboBoxDepósito = new ComboBox();
             label3 = new Label();
             botonAceptar = new Button();
-            textBoxFechaOrden = new TextBox();
-            label1 = new Label();
             textBoxCódigoOrden = new TextBox();
             label2 = new Label();
             groupBoxCliente = new GroupBox();
@@ -52,168 +41,81 @@
             labelRazonSocial = new Label();
             textBoxCUITCliente = new TextBox();
             labelCUITCliente = new Label();
+            label7 = new Label();
+            textBoxCantidad1 = new TextBox();
+            label9 = new Label();
+            label4 = new Label();
+            textBoxFechaEntrega = new TextBox();
             groupBoxOrdenPreparación.SuspendLayout();
             groupBoxCliente.SuspendLayout();
             SuspendLayout();
             // 
             // groupBoxOrdenPreparación
             // 
-            groupBoxOrdenPreparación.Controls.Add(checkedListBox2);
-            groupBoxOrdenPreparación.Controls.Add(checkedListBox1);
-            groupBoxOrdenPreparación.Controls.Add(textBox2);
-            groupBoxOrdenPreparación.Controls.Add(label8);
-            groupBoxOrdenPreparación.Controls.Add(label5);
-            groupBoxOrdenPreparación.Controls.Add(Cantid);
-            groupBoxOrdenPreparación.Controls.Add(label6);
+            groupBoxOrdenPreparación.Controls.Add(label7);
+            groupBoxOrdenPreparación.Controls.Add(textBoxCantidad1);
+            groupBoxOrdenPreparación.Controls.Add(label9);
             groupBoxOrdenPreparación.Controls.Add(textBoxFechaEntrega);
             groupBoxOrdenPreparación.Controls.Add(label4);
             groupBoxOrdenPreparación.Controls.Add(comboBoxDepósito);
             groupBoxOrdenPreparación.Controls.Add(label3);
             groupBoxOrdenPreparación.Controls.Add(botonAceptar);
-            groupBoxOrdenPreparación.Controls.Add(textBoxFechaOrden);
-            groupBoxOrdenPreparación.Controls.Add(label1);
             groupBoxOrdenPreparación.Controls.Add(textBoxCódigoOrden);
             groupBoxOrdenPreparación.Controls.Add(label2);
-            groupBoxOrdenPreparación.Location = new Point(11, 122);
+            groupBoxOrdenPreparación.Location = new Point(16, 203);
+            groupBoxOrdenPreparación.Margin = new Padding(4, 5, 4, 5);
             groupBoxOrdenPreparación.Name = "groupBoxOrdenPreparación";
-            groupBoxOrdenPreparación.Size = new Size(379, 343);
+            groupBoxOrdenPreparación.Padding = new Padding(4, 5, 4, 5);
+            groupBoxOrdenPreparación.Size = new Size(541, 572);
             groupBoxOrdenPreparación.TabIndex = 7;
             groupBoxOrdenPreparación.TabStop = false;
             groupBoxOrdenPreparación.Text = "Orden de Entrega";
             groupBoxOrdenPreparación.Enter += groupBoxOrdenPreparación_Enter;
             // 
-            // checkedListBox2
-            // 
-            checkedListBox2.FormattingEnabled = true;
-            checkedListBox2.Location = new Point(190, 134);
-            checkedListBox2.Margin = new Padding(2);
-            checkedListBox2.Name = "checkedListBox2";
-            checkedListBox2.Size = new Size(176, 112);
-            checkedListBox2.TabIndex = 24;
-            // 
-            // checkedListBox1
-            // 
-            checkedListBox1.FormattingEnabled = true;
-            checkedListBox1.Location = new Point(6, 134);
-            checkedListBox1.Margin = new Padding(2);
-            checkedListBox1.Name = "checkedListBox1";
-            checkedListBox1.Size = new Size(176, 112);
-            checkedListBox1.TabIndex = 23;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(6, 300);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(176, 23);
-            textBox2.TabIndex = 21;
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new Point(7, 283);
-            label8.Name = "label8";
-            label8.Size = new Size(27, 15);
-            label8.TabIndex = 22;
-            label8.Text = "DNI";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(7, 260);
-            label5.Name = "label5";
-            label5.Size = new Size(125, 15);
-            label5.TabIndex = 20;
-            label5.Text = "Datos del transportista";
-            // 
-            // Cantid
-            // 
-            Cantid.AutoSize = true;
-            Cantid.Location = new Point(188, 117);
-            Cantid.Name = "Cantid";
-            Cantid.Size = new Size(55, 15);
-            Cantid.TabIndex = 13;
-            Cantid.Text = "Cantidad";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(6, 117);
-            label6.Name = "label6";
-            label6.Size = new Size(66, 15);
-            label6.TabIndex = 10;
-            label6.Text = "Mercadería";
-            // 
-            // textBoxFechaEntrega
-            // 
-            textBoxFechaEntrega.Location = new Point(188, 87);
-            textBoxFechaEntrega.Name = "textBoxFechaEntrega";
-            textBoxFechaEntrega.Size = new Size(176, 23);
-            textBoxFechaEntrega.TabIndex = 8;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(189, 70);
-            label4.Name = "label4";
-            label4.Size = new Size(97, 15);
-            label4.TabIndex = 9;
-            label4.Text = "Fecha de entrega";
-            // 
             // comboBoxDepósito
             // 
             comboBoxDepósito.FormattingEnabled = true;
-            comboBoxDepósito.Location = new Point(6, 87);
+            comboBoxDepósito.Location = new Point(9, 145);
+            comboBoxDepósito.Margin = new Padding(4, 5, 4, 5);
             comboBoxDepósito.Name = "comboBoxDepósito";
-            comboBoxDepósito.Size = new Size(176, 23);
+            comboBoxDepósito.Size = new Size(250, 33);
             comboBoxDepósito.TabIndex = 7;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(7, 70);
+            label3.Location = new Point(10, 117);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(54, 15);
+            label3.Size = new Size(85, 25);
             label3.TabIndex = 6;
             label3.Text = "Depósito";
             // 
             // botonAceptar
             // 
-            botonAceptar.Location = new Point(289, 300);
+            botonAceptar.Location = new Point(413, 500);
+            botonAceptar.Margin = new Padding(4, 5, 4, 5);
             botonAceptar.Name = "botonAceptar";
-            botonAceptar.Size = new Size(75, 23);
+            botonAceptar.Size = new Size(107, 38);
             botonAceptar.TabIndex = 4;
             botonAceptar.Text = "Aceptar";
             botonAceptar.UseVisualStyleBackColor = true;
             // 
-            // textBoxFechaOrden
-            // 
-            textBoxFechaOrden.Location = new Point(188, 43);
-            textBoxFechaOrden.Name = "textBoxFechaOrden";
-            textBoxFechaOrden.Size = new Size(176, 23);
-            textBoxFechaOrden.TabIndex = 2;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(189, 26);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 3;
-            label1.Text = "Fecha";
-            // 
             // textBoxCódigoOrden
             // 
-            textBoxCódigoOrden.Location = new Point(6, 43);
+            textBoxCódigoOrden.Location = new Point(9, 72);
+            textBoxCódigoOrden.Margin = new Padding(4, 5, 4, 5);
             textBoxCódigoOrden.Name = "textBoxCódigoOrden";
-            textBoxCódigoOrden.Size = new Size(176, 23);
+            textBoxCódigoOrden.Size = new Size(250, 31);
             textBoxCódigoOrden.TabIndex = 0;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(7, 26);
+            label2.Location = new Point(10, 43);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(98, 15);
+            label2.Size = new Size(151, 25);
             label2.TabIndex = 1;
             label2.Text = "Código de Orden";
             // 
@@ -225,71 +127,126 @@
             groupBoxCliente.Controls.Add(labelRazonSocial);
             groupBoxCliente.Controls.Add(textBoxCUITCliente);
             groupBoxCliente.Controls.Add(labelCUITCliente);
-            groupBoxCliente.Location = new Point(12, 12);
+            groupBoxCliente.Location = new Point(17, 20);
+            groupBoxCliente.Margin = new Padding(4, 5, 4, 5);
             groupBoxCliente.Name = "groupBoxCliente";
-            groupBoxCliente.Size = new Size(378, 104);
+            groupBoxCliente.Padding = new Padding(4, 5, 4, 5);
+            groupBoxCliente.Size = new Size(540, 173);
             groupBoxCliente.TabIndex = 6;
             groupBoxCliente.TabStop = false;
             groupBoxCliente.Text = "Cliente";
             // 
             // botonAtras
             // 
-            botonAtras.Location = new Point(210, 72);
+            botonAtras.Location = new Point(300, 120);
+            botonAtras.Margin = new Padding(4, 5, 4, 5);
             botonAtras.Name = "botonAtras";
-            botonAtras.Size = new Size(75, 23);
+            botonAtras.Size = new Size(107, 38);
             botonAtras.TabIndex = 24;
             botonAtras.Text = "Atrás";
             botonAtras.UseVisualStyleBackColor = true;
             // 
             // botonSiguiente
             // 
-            botonSiguiente.Location = new Point(289, 72);
+            botonSiguiente.Location = new Point(413, 120);
+            botonSiguiente.Margin = new Padding(4, 5, 4, 5);
             botonSiguiente.Name = "botonSiguiente";
-            botonSiguiente.Size = new Size(75, 23);
+            botonSiguiente.Size = new Size(107, 38);
             botonSiguiente.TabIndex = 4;
             botonSiguiente.Text = "Siguiente";
             botonSiguiente.UseVisualStyleBackColor = true;
             // 
             // textBoxRazónSocialCliente
             // 
-            textBoxRazónSocialCliente.Location = new Point(194, 43);
+            textBoxRazónSocialCliente.Location = new Point(277, 72);
+            textBoxRazónSocialCliente.Margin = new Padding(4, 5, 4, 5);
             textBoxRazónSocialCliente.Name = "textBoxRazónSocialCliente";
-            textBoxRazónSocialCliente.Size = new Size(176, 23);
+            textBoxRazónSocialCliente.Size = new Size(250, 31);
             textBoxRazónSocialCliente.TabIndex = 2;
             // 
             // labelRazonSocial
             // 
             labelRazonSocial.AutoSize = true;
-            labelRazonSocial.Location = new Point(189, 26);
+            labelRazonSocial.Location = new Point(270, 43);
+            labelRazonSocial.Margin = new Padding(4, 0, 4, 0);
             labelRazonSocial.Name = "labelRazonSocial";
-            labelRazonSocial.Size = new Size(73, 15);
+            labelRazonSocial.Size = new Size(112, 25);
             labelRazonSocial.TabIndex = 3;
             labelRazonSocial.Text = "Razón Social";
             // 
             // textBoxCUITCliente
             // 
-            textBoxCUITCliente.Location = new Point(6, 43);
+            textBoxCUITCliente.Location = new Point(9, 72);
+            textBoxCUITCliente.Margin = new Padding(4, 5, 4, 5);
             textBoxCUITCliente.Name = "textBoxCUITCliente";
-            textBoxCUITCliente.Size = new Size(176, 23);
+            textBoxCUITCliente.Size = new Size(250, 31);
             textBoxCUITCliente.TabIndex = 0;
             // 
             // labelCUITCliente
             // 
             labelCUITCliente.AutoSize = true;
-            labelCUITCliente.Location = new Point(7, 26);
+            labelCUITCliente.Location = new Point(10, 43);
+            labelCUITCliente.Margin = new Padding(4, 0, 4, 0);
             labelCUITCliente.Name = "labelCUITCliente";
-            labelCUITCliente.Size = new Size(32, 15);
+            labelCUITCliente.Size = new Size(49, 25);
             labelCUITCliente.TabIndex = 1;
             labelCUITCliente.Text = "CUIT";
             // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(273, 194);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(83, 25);
+            label7.TabIndex = 26;
+            label7.Text = "Cantidad";
+            // 
+            // textBoxCantidad1
+            // 
+            textBoxCantidad1.Location = new Point(11, 224);
+            textBoxCantidad1.Margin = new Padding(4, 5, 4, 5);
+            textBoxCantidad1.Multiline = true;
+            textBoxCantidad1.Name = "textBoxCantidad1";
+            textBoxCantidad1.Size = new Size(508, 266);
+            textBoxCantidad1.TabIndex = 25;
+            textBoxCantidad1.TextChanged += textBoxCantidad1_TextChanged;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(8, 194);
+            label9.Margin = new Padding(4, 0, 4, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(79, 25);
+            label9.TabIndex = 24;
+            label9.Text = "Ordenes";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(270, 41);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(147, 25);
+            label4.TabIndex = 9;
+            label4.Text = "Fecha de entrega";
+            // 
+            // textBoxFechaEntrega
+            // 
+            textBoxFechaEntrega.Location = new Point(269, 69);
+            textBoxFechaEntrega.Margin = new Padding(4, 5, 4, 5);
+            textBoxFechaEntrega.Name = "textBoxFechaEntrega";
+            textBoxFechaEntrega.Size = new Size(250, 31);
+            textBoxFechaEntrega.TabIndex = 8;
+            // 
             // OrdenDeEntregaForms
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(405, 482);
+            ClientSize = new Size(579, 803);
             Controls.Add(groupBoxOrdenPreparación);
             Controls.Add(groupBoxCliente);
-            Margin = new Padding(2);
             Name = "OrdenDeEntregaForms";
             Text = "OrdenDeEntrega";
             groupBoxOrdenPreparación.ResumeLayout(false);
@@ -302,18 +259,9 @@
         #endregion
 
         private GroupBox groupBoxOrdenPreparación;
-        private TextBox textBox2;
-        private Label label8;
-        private Label label5;
-        private Label Cantid;
-        private Label label6;
-        private TextBox textBoxFechaEntrega;
-        private Label label4;
         private ComboBox comboBoxDepósito;
         private Label label3;
         private Button botonAceptar;
-        private TextBox textBoxFechaOrden;
-        private Label label1;
         private TextBox textBoxCódigoOrden;
         private Label label2;
         private GroupBox groupBoxCliente;
@@ -323,7 +271,10 @@
         private Label labelRazonSocial;
         private TextBox textBoxCUITCliente;
         private Label labelCUITCliente;
-        private CheckedListBox checkedListBox2;
-        private CheckedListBox checkedListBox1;
+        private Label label7;
+        private TextBox textBoxCantidad1;
+        private Label label9;
+        private TextBox textBoxFechaEntrega;
+        private Label label4;
     }
 }
