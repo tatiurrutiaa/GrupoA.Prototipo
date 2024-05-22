93adcf82fdb36b089be93c515772d785ef7bffea
FERNANDO JOSE MARTINEZ CKACKA <58MA43406426@campus.economicas.uba.ar>
2024-05-22 15:41:05 -0300

    a

---
+++ Empaquetar/EmpaquetadoForms.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace GrupoA.Prototipo.Empaquetar
+{
+    partial class EmpaquetadoForms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            EmpaquetarlistBox = new ListBox();
+            EmpaquetarButton = new Button();
+            SuspendLayout();
+            // 
+            // EmpaquetarlistBox
+            // 
+            EmpaquetarlistBox.FormattingEnabled = true;
+            EmpaquetarlistBox.ItemHeight = 15;
+            EmpaquetarlistBox.Location = new Point(12, 30);
+            EmpaquetarlistBox.Name = "EmpaquetarlistBox";
+            EmpaquetarlistBox.Size = new Size(247, 94);
+            EmpaquetarlistBox.TabIndex = 0;
+            // 
+            // EmpaquetarButton
+            // 
+            EmpaquetarButton.Location = new Point(172, 133);
+            EmpaquetarButton.Name = "EmpaquetarButton";
+            EmpaquetarButton.Size = new Size(85, 25);
+            EmpaquetarButton.TabIndex = 1;
+            EmpaquetarButton.Text = "Empaquetar";
+            EmpaquetarButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(289, 183);
+            Controls.Add(EmpaquetarButton);
+            Controls.Add(EmpaquetarlistBox);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox EmpaquetarlistBox;
+        private Button EmpaquetarButton;
+    }
+}
---
+++ Empaquetar/EmpaquetadoForms.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrupoA.Prototipo.Empaquetar
+{
+    public partial class EmpaquetadoForms : Form
+    {
+        public EmpaquetadoForms()
+        {
+            InitializeComponent();
+
+            // Crear órdenes de ejemplo
+            var orden1 = new Orden("Orden 1", new List<string> { "Item A", "Item B", "Item C" });
+            var orden2 = new Orden("Orden 2", new List<string> { "Item D", "Item E" });
+            var orden3 = new Orden("Orden 3", new List<string> { "Item F", "Item G", "Item H" });
+
+            // Agregar órdenes al ListBox
+            EmpaquetarlistBox.Items.Add(orden1);
+            EmpaquetarlistBox.Items.Add(orden2);
+            EmpaquetarlistBox.Items.Add(orden3);
+
+            // Configurar el evento Click del botón
+            EmpaquetarButton.Click += EmpaquetarButton_Click;
+        }
+        private void EmpaquetarButton_Click(object sender, EventArgs e)
+        {
+            if (EmpaquetarlistBox.SelectedItem != null)
+            {
+                Orden selectedOrder = (Orden)EmpaquetarlistBox.SelectedItem;
+                string items = string.Join(", ", selectedOrder.Items);
+                MessageBox.Show($"La orden '{selectedOrder.Nombre}' con los items [{items}] se va a empaquetar.");
+                EmpaquetarlistBox.Items.Remove(selectedOrder);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona una orden de la lista.");
+            }
+        }
+
+    }
+
+}
